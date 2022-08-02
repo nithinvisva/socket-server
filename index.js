1cd469d41d0692075c292d4e07d8aa2dea2ec227
nithinvisva <visvanic@gmailcom>
2022-08-02 13:46:53 +0530

    socket userb disabling

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 const app = require('express')();
 const httpServer = require('http').createServer(app);
 const io = require('socket.io')(httpServer, {
-  cors: {origin : '*'}
+  cors: { origin: '*' }
 });
 
 const port = process.env.PORT || 3000;
-let socketId;
-let xCount =0;
-let oCount= 0;
+let socketId = { X: null, Y: null }
+let xCount = 0;
+let oCount = 0;
 
 io.on('connection', (socket) => {
   console.log('a user connected');
   socket.on('message', (data) => {
-    data.userId= socket.id
-    if(!socketId){
-      socketId = socket.id
+    data.userId = socket.id
+    if (!socketId.X) {
+      socketId.X =  data.userId
+    }else if(socketId.X != data.userId && !socketId.Y){
+      socketId.Y =  data.userId
     }
-    if(socketId == data.userId){
-      data.value ='X';
+    console.log(`xid ${socketId.X} , x ${xCount}, y ${oCount}`)
+    if (socketId.X == data.userId && xCount == oCount) {
+      data.value = 'X';
       xCount++;
-    }else{
-      data.value ='O';
-      oCount++;
+      io.emit('message', data);
     }
-    io.emit('message', data);
+    console.log(`xid ${socketId.Y} , x ${xCount}, y ${oCount}`)
+    if (socketId.Y == data.userId && xCount > oCount) {
+      data.value = 'O';
+      oCount++;
+      io.emit('message', data);
+    }
   });
   socket.on('disconnect', () => {
     console.log('a user disconnected!');
-    socketId = null
+    socketId.X = null;
+    socketId.Y = null;
   });
 });
 
